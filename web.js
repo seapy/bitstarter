551d2f0e61656faee9f66bb4ae48b47d7a40d185
seapy <iamseapy@gmail.com>
2013-07-09 04:04:40 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var indexfile = fs.readFileSync("index.html");
+  response.send(indexfile.toString());
 });
 
 var port = process.env.PORT || 5000;
